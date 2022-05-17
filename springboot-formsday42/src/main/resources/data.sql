deb8325391f77b714ecb418f9fc129965ff718e6
bharadwajpudi <bharadwajpudi2018@gmail.com>
2022-05-17 10:28:28 +0530

    Springboot-formsday2 done

---
+++ springboot-formsday42/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into emp(ename,sal,deptno) values('King',5000,10);
+insert into emp(ename,sal,deptno) values('James',4000,10);
+insert into emp(ename,sal,deptno) values('Ward',4500,20);
+insert into emp(ename,sal,deptno) values('Smith',3000,30);
+insert into emp(ename,sal,deptno) values('Jones',3500,40);
+insert into emp(ename,sal,deptno) values('Martin',2000,40);
---
+++ springboot-formsday42/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table emp(empno integer not null auto_increment,
+ename varchar(50) not null,
+sal double,
+deptno integer,
+primary key(empno));
